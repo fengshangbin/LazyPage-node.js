88b258f30a3e162a4825ac255517d8168db42d5b
shangbin <fengshangbin@126.com>
2020-04-30 18:31:30 +0800

    update

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-"use strict";
+'use strict';
 
-var parseUrl = require("parseurl");
-var fs = require("fs");
-var pathNode = require("path");
-var template = require("./lib2/template-web");
-var analyzeHtml = require("./lib2/analyzeHtml");
-var queryLazyPageSelector = require("./lib/queryLazyPage");
-var queryhelp = require("./lib/queryhelp").QueryHelp;
+var parseUrl = require('parseurl');
+var fs = require('fs');
+var pathNode = require('path');
+var template = require('./lib/template-web');
+var analyzeHtml = require('./lib/analyzeHtml');
+var queryLazyPageSelector = require('./lib/queryLazyPage');
 
 var mapping;
 let htmlPaths = new Set();
@@ -25,54 +24,62 @@ module.exports = {
 /* function host(_realHost) {
   realHost = _realHost;
 } */
-function loadjs(code, alias){
+/* function loadjs(code, alias) {
+  console.log('---------------', code);
   template.defaults.imports.$import[alias] = code;
-}
+} */
 
-function loadconfig(root){
-  try{
-    var data=fs.readFileSync(root + "/config.json", 'utf-8');
-    try{
+function loadconfig(root) {
+  try {
+    var data = fs.readFileSync(root + '/config.json', 'utf-8');
+    try {
       let config = JSON.parse(data);
-      if(config.mapping && config.mapping.length>0){
+      if (config.mapping && config.mapping.length > 0) {
         mapping = config.mapping;
       }
-      if(config.import){
+      if (config.import) {
         template.defaults.imports.$import = {};
-        for(var key in config.import){
-          let path = config.import[key];
-          try{
-            var code=fs.readFileSync(root+"/"+path, 'utf-8');
-            loadjs(eval(code), key);
-            /* loadjs(require(__dirname+"/"+root+"/"+js.path), js.alias); */
-          }catch(e){
-            console.log("error js: "+path, e);
+        for (var i = 0; i < config.import.length; i++) {
+          let path = config.import[i];
+          try {
+            var code = fs.readFileSync(root + '/' + path, 'utf-8');
+            code = '(function(root) {' + code + '})(template.defaults.imports.$import);';
+            eval(code);
+          } catch (e) {
+            console.log('error js: ' + path, e);
           }
+          /* let path = config.import[key];
+          try {
+            var code = fs.readFileSync(root + '/' + path, 'utf-8');
+            loadjs(eval(code), key);
+          } catch (e) {
+            console.log('error js: ' + path, e);
+          } */
         }
       }
-      if(config.config){
-        var set ={};
+      if (config.config) {
+        var set = {};
         Object.assign(set, config.config);
         template.defaults.imports.$config = set;
       }
-    }catch(e){
-      console.error("config.json need json format!");
+    } catch (e) {
+      console.error('config.json need json format!');
     }
-  }catch(e){}
+  } catch (e) {}
 }
 
 function route(root) {
   clearMap();
-  if (typeof root == "string") {
+  if (typeof root == 'string') {
     let rootLen = root.length;
     readDirSync(root);
     function readDirSync(pathStr) {
       var files = fs.readdirSync(pathStr);
-      files.forEach(function (ele, index) {
+      files.forEach(function(ele, index) {
         let filePath = pathNode.join(pathStr, ele);
         let info = fs.statSync(filePath);
         if (info.isDirectory()) {
-          if (ele != "node_modules") readDirSync(filePath);
+          if (ele != 'node_modules') readDirSync(filePath);
         } else {
           addMap(filePath.substring(rootLen));
         }
@@ -80,7 +87,7 @@ function route(root) {
     }
   } else {
     assets = root;
-    Object.keys(assets).forEach((key) => {
+    Object.keys(assets).forEach(key => {
       addMap(pathNode.sep + key);
     });
   }
@@ -93,7 +100,7 @@ function clearMap() {
 }
 
 function sortMap() {
-  map.sort(function (a, b) {
+  map.sort(function(a, b) {
     return a.sort < b.sort ? 1 : -1;
   });
   //console.log(map);
@@ -102,22 +109,22 @@ function sortMap() {
 function addMap(filePath) {
   //console.log(filePah)
   var fileName = pathNode.win32.basename(filePath);
-  if (!fileName.startsWith("_") && fileName.endsWith(".html")) {
-    let reg = new RegExp(pathNode.sep + pathNode.sep, "g");
-    let realPath = filePath.replace(reg, "/");
-    let routePath = realPath.replace("-.html", "");
-    routePath = routePath.replace(/\+/g, "/");
-    routePath = routePath.replace(/\$/g, "([^/]*?)");
+  if (!fileName.startsWith('_') && fileName.endsWith('.html')) {
+    let reg = new RegExp(pathNode.sep + pathNode.sep, 'g');
+    let realPath = filePath.replace(reg, '/');
+    let routePath = realPath.replace('-.html', '');
+    routePath = routePath.replace(/\+/g, '/');
+    routePath = routePath.replace(/\$/g, '([^/]*?)');
     if (routePath != realPath) {
       var sort = 0;
       for (var i = 0; i < realPath.length; i++) {
         var str = realPath.charAt(i);
-        sort += (str == "$" ? 1 : 2) * 10 * (realPath.length - i);
+        sort += (str == '$' ? 1 : 2) * 10 * (realPath.length - i);
       }
       map.push({
-        key: "^" + routePath + "$",
+        key: '^' + routePath + '$',
         value: realPath,
-        sort: sort,
+        sort: sort
       });
     } else {
       htmlPaths.add(realPath);
@@ -133,23 +140,23 @@ function response(root) {
     var cookies = req.headers.cookie;
     var pathParams = null;
     var ext = pathNode.extname(path);
-    ext = ext.length > 0 ? ext.slice(1) : "unknown";
+    ext = ext.length > 0 ? ext.slice(1) : 'unknown';
     var fileName = pathNode.basename(path);
     var hitHtml = false;
-    if (ext != "unknown" && ext != "html") {
+    if (ext != 'unknown' && ext != 'html') {
       next();
-    } else if (fileName.startsWith("_") && ext == "html") {
+    } else if (fileName.startsWith('_') && ext == 'html') {
       next();
     } else {
       if (htmlPaths.has(path)) {
         hitHtml = true;
-      } else if (path.endsWith("/") && htmlPaths.has(path + "index.html")) {
+      } else if (path.endsWith('/') && htmlPaths.has(path + 'index.html')) {
         hitHtml = true;
-        path = path + "index.html";
+        path = path + 'index.html';
       } else {
         for (var i = 0; i < map.length; i++) {
           var key = map[i].key;
-          let reg = new RegExp(key, "i");
+          let reg = new RegExp(key, 'i');
           if (reg.test(path)) {
             let group = reg.exec(path);
             path = map[i].value.substring(1);
@@ -163,8 +170,8 @@ function response(root) {
       }
       if (hitHtml) {
         //console.log(path);
-        if (root && typeof root == "string") {
-          fromDir(root, path, function (html) {
+        if (root && typeof root == 'string') {
+          fromDir(root, path, function(html) {
             lazypage(req, res, html, pathParams, cookies, next);
           });
         } else {
@@ -178,10 +185,10 @@ function response(root) {
 }
 
 function fromDir(root, path, callback) {
-  fs.readFile(root + "/" + path, "utf-8", function (err, html) {
+  fs.readFile(root + '/' + path, 'utf-8', function(err, html) {
     if (err) {
       console.log(err);
-      next("code 404, file not found");
+      next('code 404, file not found');
     } else {
       callback(html);
     }
@@ -189,40 +196,38 @@ function fromDir(root, path, callback) {
 }
 
 function lazypage(req, res, html, pathParams, cookies, next) {
-  var fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
-  var urls = fullUrl.split("?");
+  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  var urls = fullUrl.split('?');
   //query = query.length > 1 ? query[1] : null;
   try {
     //console.log(mapping, urls[0], urls[1]);
-    new analyzeHtml().parse(mapping, urls[0], urls[1], html, cookies, function (
-      code,
-      result
-    ) {
+    new analyzeHtml().parse(mapping, urls[0], urls[1], html, cookies, function(code, result) {
       //, pathParams
       if (code == 200) {
         if (req.query.lazypageTargetSelector) {
-          var block = queryLazyPageSelector(
-            result,
-            req.query.lazypageTargetSelector
-          );
+          var block = queryLazyPageSelector(result, req.query.lazypageTargetSelector);
           var resultJSON = {
-            block: block,
-            hasTargetLazyPage: block != null,
+            block: block != null ? block.getOuterHTML().replace(/ lazypagelevel\d/g, '') : null,
+            hasTargetLazyPage: block != null
           };
           if (block) {
-            resultJSON.title = queryhelp.querySelector(result, "title");
-          }
+            resultJSON.title = result.querySelector('title').getInnerHTML();
+          } /*  else {
+            console.log(urls[0], req.query.lazypageTargetSelector);
+          } */
           result = JSON.stringify(resultJSON);
+        } else {
+          result = result.html.replace(/(\r|\n)( *(\r|\n))+/g, '\r');
         }
         render(req, res, result);
       } else {
         console.log(result);
-        next("server error");
+        next('server error');
       }
     });
   } catch (error) {
     console.log(error);
-    next("server error");
+    next('server error');
   }
 }
 
@@ -234,7 +239,7 @@ function filter(root) {
 
 function render(req, res, doc) {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/html; charset=UTF-8");
-  res.setHeader("Content-Length", Buffer.byteLength(doc));
+  res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+  res.setHeader('Content-Length', Buffer.byteLength(doc));
   res.end(doc);
 }
--- test/static/js/format.js
+++ test/static/js/format.js
@@ -1,81 +1,100 @@
-var format = {
-	//千位数字逗号分隔
-	toThousands: function(number){
-		var num = (number || 0).toString();
-		var nums=num.split(".");
-		nums[0]=nums[0].replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g,'$&,');
-		return nums.join(".");
-	},
-	//时间戳(秒)日期格式化
-	dataFormat: function(formatStr, timeStamp) {  
-		var date = new Date(timeStamp*1000);
-		var zeroize = function (value, length) {  
-			if (!length) {  
-				length = 2;  
-			}  
-			value = new String(value);  
-			for (var i = 0, zeros = ''; i < (length - value.length); i++) {  
-				zeros += '0';  
-			}  
-				return zeros + value;  
-		};  
-		var timeago = function(){
-			var dateTimeStamp = date.getTime();
-			var minute = 1000 * 60;
-			var hour = minute * 60;
-			var day = hour * 24;
+root.format = {
+  //千位数字逗号分隔
+  toThousands: function(number) {
+    var num = (number || 0).toString();
+    var nums = num.split('.');
+    nums[0] = nums[0].replace(/\d{1,3}(?=(\d{3})+(\.\d*)?$)/g, '$&,');
+    return nums.join('.');
+  },
+  //时间戳(秒)日期格式化
+  dataFormat: function(formatStr, timeStamp) {
+    var date = new Date(timeStamp * 1000);
+    var zeroize = function(value, length) {
+      if (!length) {
+        length = 2;
+      }
+      value = new String(value);
+      for (var i = 0, zeros = ''; i < length - value.length; i++) {
+        zeros += '0';
+      }
+      return zeros + value;
+    };
+    var timeago = function() {
+      var dateTimeStamp = date.getTime();
+      var minute = 1000 * 60;
+      var hour = minute * 60;
+      var day = hour * 24;
 
-			var now = new Date().getTime();
-			var diffValue = now - dateTimeStamp;
+      var now = new Date().getTime();
+      var diffValue = now - dateTimeStamp;
 
-			var minC = diffValue/minute;
-			var hourC = diffValue/hour;
-			var dayC = diffValue/day;
-			var secondC = diffValue/1000;
+      var minC = diffValue / minute;
+      var hourC = diffValue / hour;
+      var dayC = diffValue / day;
+      var secondC = diffValue / 1000;
 
-			if(dayC >= 1 && dayC <2){
-				result = parseInt(dayC) + " day ago"
-			}else if(hourC >= 1 && hourC < 24){
-				result = parseInt(hourC) + " hour"+(parseInt(hourC)>1?"s":"")+" ago"
-			}else if(minC >= 1 && minC< 60){
-				result =parseInt(minC) + " minute"+(parseInt(minC)>1?"s":"")+" ago"
-			}else if(secondC >= 10 && secondC < 60){
-				result =parseInt(secondC) + " seconds ago"
-			}else if(diffValue >= 0 && secondC < 10){
-				result = "just now"
-			}else {
-				result = formatStr.replace(/"[^"]*"|'[^']*'|\b(?:d{1,4}|M{1,4}|yy(?:yy)?|([hHmstT])\1?|[lLZ])\b/g, function($0) {  
-					switch ($0) {  
-						case 'd': return date.getDate();  
-						case 'dd': return zeroize(date.getDate());  
-						case 'ddd': return ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'][date.getDay()];  
-						case 'dddd': return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][date.getDay()];  
-						case 'M': return date.getMonth() + 1;  
-						case 'MM': return zeroize(date.getMonth() + 1);  
-						case 'MMM': return ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][date.getMonth()];  
-						case 'MMMM': return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][date.getMonth()];  
-						case 'yy': return new String(date.getFullYear()).substr(2);  
-						case 'yyyy': return date.getFullYear();  
-						case 'h': return date.getHours() % 12 || 12;  
-						case 'hh': return zeroize(date.getHours() % 12 || 12);  
-						case 'H': return date.getHours();  
-						case 'HH': return zeroize(date.getHours());  
-						case 'm': return date.getMinutes();  
-						case 'mm': return zeroize(date.getMinutes());  
-						case 's': return date.getSeconds();  
-						case 'ss': return zeroize(date.getSeconds());  
-						case 'l': return date.getMilliseconds();  
-						case 'll': return zeroize(date.getMilliseconds());  
-						case 'tt': return date.getHours() < 12 ? 'am' : 'pm';  
-						case 'TT': return date.getHours() < 12 ? 'AM' : 'PM';  
-					}  
-				})
-			}
-			return result;
-		}
-		return timeago();  
-	}
-}
-if(typeof module !== 'undefined'){
-	module.exports=format;
-}
+      if (dayC >= 1 && dayC < 2) {
+        result = parseInt(dayC) + ' day ago';
+      } else if (hourC >= 1 && hourC < 24) {
+        result = parseInt(hourC) + ' hour' + (parseInt(hourC) > 1 ? 's' : '') + ' ago';
+      } else if (minC >= 1 && minC < 60) {
+        result = parseInt(minC) + ' minute' + (parseInt(minC) > 1 ? 's' : '') + ' ago';
+      } else if (secondC >= 10 && secondC < 60) {
+        result = parseInt(secondC) + ' seconds ago';
+      } else if (diffValue >= 0 && secondC < 10) {
+        result = 'just now';
+      } else {
+        result = formatStr.replace(/"[^"]*"|'[^']*'|\b(?:d{1,4}|M{1,4}|yy(?:yy)?|([hHmstT])\1?|[lLZ])\b/g, function($0) {
+          switch ($0) {
+            case 'd':
+              return date.getDate();
+            case 'dd':
+              return zeroize(date.getDate());
+            case 'ddd':
+              return ['Sun', 'Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat'][date.getDay()];
+            case 'dddd':
+              return ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][date.getDay()];
+            case 'M':
+              return date.getMonth() + 1;
+            case 'MM':
+              return zeroize(date.getMonth() + 1);
+            case 'MMM':
+              return ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'][date.getMonth()];
+            case 'MMMM':
+              return ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'][date.getMonth()];
+            case 'yy':
+              return new String(date.getFullYear()).substr(2);
+            case 'yyyy':
+              return date.getFullYear();
+            case 'h':
+              return date.getHours() % 12 || 12;
+            case 'hh':
+              return zeroize(date.getHours() % 12 || 12);
+            case 'H':
+              return date.getHours();
+            case 'HH':
+              return zeroize(date.getHours());
+            case 'm':
+              return date.getMinutes();
+            case 'mm':
+              return zeroize(date.getMinutes());
+            case 's':
+              return date.getSeconds();
+            case 'ss':
+              return zeroize(date.getSeconds());
+            case 'l':
+              return date.getMilliseconds();
+            case 'll':
+              return zeroize(date.getMilliseconds());
+            case 'tt':
+              return date.getHours() < 12 ? 'am' : 'pm';
+            case 'TT':
+              return date.getHours() < 12 ? 'AM' : 'PM';
+          }
+        });
+      }
+      return result;
+    };
+    return timeago();
+  }
+};
