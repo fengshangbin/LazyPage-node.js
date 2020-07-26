075bb8baee7fa604b063b1e2555c702bc3216180
fengshangbin <fengshangbin@126.com>
2020-07-26 19:26:15 +0800

    update

--- core/pathUtils.js
+++ core/pathUtils.js
@@ -1,52 +1,70 @@
 function getDomain(url) {
-    if (url == null) return location.origin;
-    var group = /^((https|http|ftp|rtsp|mms|file)?:\/\/[^/]*)/i.exec(url);
-    return group ? group[0] : null;
+  if (url == null) return location.origin;
+  var group = /^((https|http|ftp|rtsp|mms|file)?:\/\/[^/]*)/i.exec(url);
+  return group ? group[0] : null;
 }
 
 function getPath(url) {
-    var domain = getDomain(url);
-    return url.replace(new RegExp(domain, 'i'), '').replace(/(\?.*)|(#.*)/, '').replace(/\/\//g, '/');
+  var domain = getDomain(url);
+  return url
+    .replace(new RegExp(domain, "i"), "")
+    .replace(/(\?.*)|(#.*)/, "")
+    .replace(/\/\//g, "/");
 }
 
 function getPaths(url) {
-    var paths = getPath(url).replace(/^\//, '').split('/');
-    return paths;
+  var paths = getPath(url).replace(/^\//, "").split("/");
+  return paths;
 }
 
 function getPathsWithIgnorePath(url, ignorePath) {
-    var paths = getPath(url).replace(new RegExp("^/?" + ignorePath + "/?", "i"), "").split('/');
-    return paths;
+  var paths = getPath(url)
+    .replace(/^\//, "")
+    .replace(new RegExp("^/?" + ignorePath + "/?", "i"), "")
+    .split("/");
+  return paths;
 }
 
 function getFinalURL(currentURL, targetURL) {
-    if (checkUrl(targetURL)) return targetURL;
-    else {
-        var domain = getDomain(currentURL);
-        var paths = getPaths(currentURL);
+  if (checkUrl(targetURL)) return targetURL;
+  else {
+    var domain = getDomain(currentURL);
+    var paths = getPaths(currentURL);
+    paths.pop();
+    if (targetURL.startsWith("/")) {
+      return domain + targetURL;
+    } else if (targetURL.startsWith("../")) {
+      while (targetURL.startsWith("../")) {
+        targetURL = targetURL.substring(3);
         paths.pop();
-        if (targetURL.startsWith('/')) {
-            return domain + targetURL;
-        } else if (targetURL.startsWith('../')) {
-            while (targetURL.startsWith('../')) {
-                targetURL = targetURL.substring(3);
-                paths.pop();
-            }
-            return domain + "/" + paths.join("/") + (paths.length > 0 ? "/" : "") + targetURL;
-        } else {
-            return domain + "/" + paths.join("/") + (paths.length > 0 ? "/" : "") + targetURL;
-        }
+      }
+      return (
+        domain +
+        "/" +
+        paths.join("/") +
+        (paths.length > 0 ? "/" : "") +
+        targetURL
+      );
+    } else {
+      return (
+        domain +
+        "/" +
+        paths.join("/") +
+        (paths.length > 0 ? "/" : "") +
+        targetURL
+      );
     }
+  }
 }
 function checkUrl(url) {
-    var regex = '^((https|http|ftp|rtsp|mms|file)?://)(.*?)';
-    var pattern = new RegExp(regex, 'i');
-    return pattern.test(url);
+  var regex = "^((https|http|ftp|rtsp|mms|file)?://)(.*?)";
+  var pattern = new RegExp(regex, "i");
+  return pattern.test(url);
 }
 module.exports = {
-    getDomain,
-    getPath,
-    getPaths,
-    getPathsWithIgnorePath,
-    getFinalURL
+  getDomain,
+  getPath,
+  getPaths,
+  getPathsWithIgnorePath,
+  getFinalURL,
 };
